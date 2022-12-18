8a7dd42821682a443bd9b69e6275fe65c5ea27c7
thuha <thuhavothi2001@gmail.com>
2022-12-18 18:44:01 +0300

    modified environment variables

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
     SECRET_KEY = 'Hello, my name is Ha'
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    db_hostname = os.environ.get('POSTGRES_HOSTNAME')
+    db_username = os.environ.get('POSTGRES_USERNAME')
+    db_password = os.environ.get('POSTGRES_PASSWORD')
+    db_database = os.environ.get('POSTGRES_DATABASE')
+    db_port = os.environ.get('POSTGRES_PORT')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(db_username, db_password, db_hostname,
+                                                                                 db_port, db_database)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
