ca4c8e53f9825c1d208586dc2e5ce7fd227fa9ed
Ha Vo <84851139+havo2001@users.noreply.github.com>
2022-12-20 00:41:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     db_hostname = os.environ.get('POSTGRES_HOSTNAME')
     db_username = os.environ.get('POSTGRES_USERNAME')
     db_password = os.environ.get('POSTGRES_PASSWORD')
-    db_database = os.environ.get('POSTGRES_DATABASE')
+    db_database = os.environ.get('POSTGRES_DB')
     db_port = os.environ.get('POSTGRES_PORT')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(db_username, db_password, db_hostname,
                                                                                  db_port, db_database)
